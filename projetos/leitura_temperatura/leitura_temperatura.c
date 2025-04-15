2a1df97b7d54c4a198312f4d4ab8be0f899baab5
Felipe Leme <92899707+fehleme97@users.noreply.github.com>
2025-04-14 23:09:43 -0300

    Atualização do código principal.
    Att

--- projetos/leitura_temperatura/leitura_temperatura.c
+++ projetos/leitura_temperatura/leitura_temperatura.c
@@ -13,17 +13,17 @@ const uint I2C_SDA = 14;
 const uint I2C_SCL = 15;
 
 #define ADC_TEMPERATURE_CHANNEL 4  
+#define NUM_AMOSTRAS 15  // Número de leituras para média móvel
 
 // Função para converter o valor lido do ADC para temperatura em Celsius
 float adc_to_temperature_celsius(uint16_t adc_value) {
-    const float conversion_factor = 3.3f / (1 << 12);  // Conversão de 12 bits (0-4095) para 0-3.3V
-    float voltage = adc_value * conversion_factor;     // Converte o valor ADC para tensão
-    float temperature_celsius = 27.0f - (voltage - 0.706f) / 0.001721f;  // Equação fornecida
+    const float conversion_factor = 3.3f / (1 << 12);  // 12 bits -> 0 a 4095
+    float voltage = adc_value * conversion_factor;
+    float temperature_celsius = 27.0f - (voltage - 0.706f) / 0.001721f;
     return temperature_celsius;
 }
 
 int main() {
-    
     stdio_init_all();
     adc_init();
 
@@ -53,25 +53,33 @@ int main() {
     render_on_display(ssd, &frame_area);
     sleep_ms(4000);
 
-    // Habilita e seleciona o sensor de temperatura interno
     adc_set_temp_sensor_enabled(true);  
     adc_select_input(ADC_TEMPERATURE_CHANNEL);  
 
     while (true) {
-        uint16_t adc_value = adc_read();
-        float temperature_celsius = adc_to_temperature_celsius(adc_value);
+        uint32_t soma_adc = 0;
+
+        // incrementa as leituras até chegar em 15 leituras e da um pequeno delay entre as amostras.
+        for (int i = 0; i < NUM_AMOSTRAS; i++) {
+            soma_adc += adc_read();
+            sleep_ms(50);  // Pequeno delay entre amostras
+        }
+
+        // média móvel para melhoria da precisão e variações.
+        uint16_t media_adc = soma_adc / NUM_AMOSTRAS;
+        float temperatura = adc_to_temperature_celsius(media_adc);
 
         memset(ssd, 0, ssd1306_buffer_length);
         char texto[32];
 
         snprintf(texto, sizeof(texto), "   TEMPERATURA ");
         ssd1306_draw_string(ssd, 0, 16, texto);
-        snprintf(texto, sizeof(texto), "       %.F C", temperature_celsius);
+        snprintf(texto, sizeof(texto), "      %.1f C", temperatura);
         ssd1306_draw_string(ssd, 0, 32, texto);
         render_on_display(ssd, &frame_area);
 
-        sleep_ms(1000);
+        sleep_ms(1000);  // Atualiza a cada segundo
     }
 
-    return 0; 
+    return 0;
 }
